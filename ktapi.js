341d5d491d4541ef427690e377b1b98e8fae5fc4
125m125 <125m125@125m125.de>
2017-09-24 21:53:15 +0200

    update to new API

--- ktapi.js
+++ ktapi.js
@@ -5,15 +5,11 @@
     "use strict";
 
 
-    var baseUrl = "https://kt.125m125.de/api/",
-        hashType = "SHA-256",
-        hmac = function (text, key) {
-            var shaObj = new jsSHA(hashType, "TEXT");
-            shaObj.setHMACKey(key, "TEXT");
-            shaObj.update(text);
-            return shaObj.getHMAC("HEX");
-        },
+    var baseUrl = "https://kt.125m125.de/api/v2.0/",
         paramsToQuery = function (params) {
+            if (!params) {
+                return params;
+            }
             var result = "",
                 sorted_keys = Object.keys(params).sort();
             sorted_keys.forEach(function (entry) {
@@ -24,141 +20,228 @@
             }
             return result;
         },
-        performRequest = function (method, type, suburl, params, token, primaryParam, callback) {
+        performRequest = function (method, type, suburl, params, headers, callback) {
             var url;
-            if (token) {
-                params.timestamp = (new Date()).getTime();
-                params.signature = hmac(paramsToQuery(params), token);
-            }
             if (method === "GET") {
                 url = baseUrl + suburl + "?" + paramsToQuery(params);
-                d3[type](url, callback);
+                params = null;
             } else {
-                if (primaryParam) {
-                    url = baseUrl + suburl + "?" + primaryParam + "=" + params[primaryParam];
-                    delete params[primaryParam];
-                } else {
-                    url = baseUrl + suburl;
+                url = baseUrl + suburl;
+                if (!headers["Content-Type"]) {
+                    headers["Content-Type"] = "application/x-www-form-urlencoded";
                 }
-                d3.request(url).header("Content-Type", "application/x-www-form-urlencoded").post(paramsToQuery(params), callback);
             }
+            var request = d3[type](url);
+            Object.keys(headers).forEach(function (key) { request.header(key, headers[key]) });
+            request.on("error", function (error) {
+                var errorTarget = error.target;
+                try {
+                    return callback(JSON.parse(errorTarget.response));
+                } catch (e) {
+                }
+                callback(errorTarget);
+            }).on("load", function (data) { callback(false, data) });
+            request.send(method, paramsToQuery(params));
         };
+    function HmacAuthenticator() {
+        this.maxSignatureOffset = 4 * 60 * 1000;
+        this.hashType = "SHA-256",
+            this.dTime = 0;
+        var last = 0;
+        this.authenticate = function (method, type, suburl, params, headers, user) {
+            params.tid = user.tid;
+            if (last) {
+                if (last < new Date().getTime() - this.maxSignatureOffset) {
+                    last = new Date().getTime() + this.maxSignatureOffset;
+                }
+                params.timestamp = last + this.dTime;
+            } else {
+                params.timestamp = (new Date()).getTime() + this.dTime;
+            }
+            params.signature = this.hmac(paramsToQuery(params), user.tkn, this.hashType);
+        }
+    }
+    HmacAuthenticator.prototype.hmac = function (text, key, hashType) {
+        var shaObj = new jsSHA(hashType, "TEXT");
+        shaObj.setHMACKey(key, "TEXT");
+        shaObj.update(text);
+        return shaObj.getHMAC("HEX");
+    }
 
-    function Kt(uid, tid, tkn) {
+    function Request(method, type, suburl, params, headers, user, authenticator) {
+        this.execute = function (callback) {
+            if (!params) {
+                params = {};
+            }
+            if (!headers) {
+                headers = {};
+            }
+            suburl = suburl.replace(/{.*?}/g, function (key) {
+                if (key === "{user}") {
+                    return user.uid;
+                }
+                var key2 = key.substring(1, key.length - 1);
+                if (params.hasOwnProperty(key2) && params[key2] !== undefined) {
+                    var result = params[key2];
+                    delete params[key2];
+                    return result;
+                }
+                return key
+            });
+            if (authenticator) {
+                authenticator.authenticate(method, type, suburl, params, headers, user);
+            }
+            performRequest(method, type, suburl, params, headers, callback);
+        }
+    }
+
+    function Kt(uid, tid, tkn, authenticator) {
         var user = {
             "uid": uid,
             "tid": tid,
             "tkn": tkn
         };
-        this.getItems = function (callback) {
-            var params = {};
-            params = {
-                uid: user.uid,
-                tid: user.tid
-            };
-            performRequest("GET", "csv", "itemlist", params, user.tkn, false, callback);
-        };
-        this.getTrades = function (callback) {
-            var params = {};
-            params = {
-                uid: user.uid,
-                tid: user.tid
-            };
-            performRequest("GET", "csv", "trades", params, user.tkn, false, callback);
-        };
-
-        this.getHistory = function (item, limit, callback) {
-            item = (typeof item === "string") ? item : item.id;
-            d3.csv(baseUrl + "history?res=" + item + "&limit=" + limit, callback);
-        };
-        this.getPrice = function (item, callback) {
-            this.getHistory(item, 1, function (data) {
-                if (data.length > 0) {
-                    callback(data[0].close);
-                } else {
-                    callback(-1);
-                }
-            });
-        };
-
-        this.getOrderBook = function (item, limit, summarize, callback) {
-            item = (typeof item === "string") ? item : item.id;
-            d3.csv(baseUrl + "order?res=" + item + "&limit=" + limit + "&summarize=" + summarize, callback);
-        };
-        this.getOrderBookPrice = function (item, buyOrSell, trumpSame, callback) {
-            this.getOrderBook(item, 1, false, function (data) {
-                var price = -1;
-                data.forEach(function (entry) {
-                    if (buyOrSell !== (entry.type === "buy")) {
-                        price = entry.price;
-                    } else if (trumpSame) {
-                        if (buyOrSell) {
-                            price = Math.max(price, Number(entry.price) + 0.01);
-                        } else {
-                            if (price === -1) {
-                                price = Number(entry.price) - 0.01;
-                            } else {
-                                price = Math.min(price, Number(entry.price) - 0.01);
-                            }
-                        }
-                    }
-                });
-                callback(price);
-            });
-        };
-
-        this.getRecommendedPrice = function (item, buyOrSell, callback) {
-            this.getOrderBookPrice(item, buyOrSell, true, function (result) {
-                if (result !== -1) {
-                    callback(result);
-                } else {
-                    this.getPrice(item, callback);
-                }
-            }.bind(this));
-        };
-
-        this.createTrade = function (buyOrSell, item, count, price, callback) {
-            var params;
-            item = (typeof item === "string") ? item : item.id;
-            params = {
-                "create": "create",
-                "bs": (buyOrSell ? "buy" : "sell"),
-                "item": item,
-                "count": count,
-                "price": price,
-                "uid": user.uid,
-                "tid": user.tid
-            };
-            performRequest("POST", false, "trades", params, user.tkn, "create", callback);
-        };
-        this.buy = function (item, count, price, callback) {
-            this.createTrade(true, item, count, price, callback);
-        };
-        this.sell = function (item, count, price, callback) {
-            this.createTrade(false, item, count, price, callback);
-        };
-        this.cancelTrade = function (trade, callback) {
-            var params;
-            trade = (typeof trade === "string") ? trade : trade.id;
-            params = {
-                "cancel": "cancel",
-                "tradeid": trade,
-                "uid": user.uid,
-                "tid": user.tid
-            };
-            performRequest("POST", false, "trades", params, user.tkn, "cancel", callback);
-        };
-        this.takeout = function (trade, callback) {
-            var params;
-            trade = (typeof trade === "string") ? trade : trade.id;
-            params = {
-                "takeout": "takeout",
-                "tradeid": trade,
-                "uid": user.uid,
-                "tid": user.tid
-            };
-            performRequest("POST", false, "trades", params, user.tkn, "takeout", callback);
-        };
+        if (!authenticator) {
+            authenticator = new HmacAuthenticator();
+        }
+        this.getRequest = function (method, type, suburl, params, headers, auth) {
+            var requestAuthenticator;
+            if (auth) {
+                requestAuthenticator = authenticator;
+            }
+            return new Request(method, type, suburl, params, headers, user, requestAuthenticator);
+        }
+        this.performRequest = function (method, type, suburl, params, headers, auth, callback) {
+            return this.getRequest(method, type, suburl, params, headers, auth).execute(callback);
+        }
     }
+    Kt.prototype.getItems = function (callback) {
+        this.performRequest("GET", "tsv", "users/{user}/items", null, null, true, callback);
+    };
+    Kt.prototype.getItem = function (item, callback) {
+        this.performRequest("GET", "tsv", "users/{user}/items/{item}", { "item": (typeof item === "string") ? item : item.id }, null, true, callback);
+    };
+    Kt.prototype.getTrades = function (callback) {
+        this.performRequest("GET", "tsv", "users/{user}/orders", null, null, true, callback);
+    };
+
+    Kt.prototype.createTrade = function (buyOrSell, item, count, price, callback) {
+        var params;
+        item = (typeof item === "string") ? item : item.id;
+        params = {
+            "buySell": (buyOrSell ? "BUY" : "SELL"),
+            "item": item,
+            "amount": count,
+            "price": price
+        };
+        this.performRequest("POST", "json", "users/{user}/orders", params, null, true, callback);
+    };
+
+    Kt.prototype.buy = function (item, count, price, callback) {
+        this.createTrade(true, item, count, price, callback);
+    };
+
+    Kt.prototype.sell = function (item, count, price, callback) {
+        this.createTrade(false, item, count, price, callback);
+    };
+
+    Kt.prototype.cancelTrade = function (trade, callback) {
+        var params;
+        trade = (typeof trade === "string") ? trade : trade.id;
+        params = {
+            "orderId": trade
+        };
+        this.performRequest("POST", "json", "users/{user}/orders/{orderId}/cancel", params, null, true, callback);
+    };
+
+    Kt.prototype.takeoutTrade = function (trade, callback) {
+        var params;
+        trade = (typeof trade === "string") ? trade : trade.id;
+        params = {
+            "orderId": trade
+        };
+        this.performRequest("POST", "json", "users/{user}/orders/{orderId}/takeout", params, null, true, callback);
+    };
+
+    Kt.prototype.pusherAuthenticate = function (socket, channel) {
+        var params;
+        params = {
+            "channel_name": channel,
+            "socketId": socket
+        };
+        this.performRequest("POST", "json", "pusher/authenticate", params, null, true, callback);
+    }
+
+    Kt.prototype.getPayouts = function (callback) {
+        performRequest("GET", "tsv", "users/{user}/payouts", null, null, true, callback);
+    };
+
+    Kt.prototype.createPayout = function (type, item, count, callback) {
+        var params;
+        item = (typeof item === "string") ? item : item.id;
+        params = {
+            "type": type,
+            "item": item,
+            "amount": amount
+        };
+        this.performRequest("POST", "json", "users/{user}/payouts", params, null, true, callback);
+    };
+
+    Kt.prototype.cancelPayout = function (payout, callback) {
+        var params;
+        payout = (typeof payout === "string") ? payout : payout.id;
+        params = {
+            "payoutid": payout
+        };
+        this.performRequest("POST", "json", "users/{user}/payouts/cancel", params, null, true, callback);
+    };
+
+    Kt.prototype.takeoutPayout = function (payout, callback) {
+        var params;
+        payout = (typeof payout === "string") ? payout : payout.id;
+        params = {
+            "payoutid": payout
+        };
+        this.performRequest("POST", "json", "users/{user}/payouts/takeout", params, null, true, callback);
+    };
+
+    Kt.prototype.getMessages = function (callback) {
+        this.performRequest("GET", "tsv", "users/{user}/messages", null, null, true, callback);
+    };
+
+    Kt.prototype.getPermissions = function(callback) {
+        this.performRequest("GET", "json", "permissions/{user}", null, null, true, callback);
+    }
+
+    Kt.prototype.getHistory = function(item, limit, offset, callback) {
+        var params;
+        item = (typeof item === "string") ? item : item.id;
+        offset = offset || 0;
+        limit = limit || 30;
+        params = {
+            "item": item,
+            "offset": offset,
+            "limit": limit
+        }
+        this.performRequest("GET", "tsv", "history/{item}", params, null, false, callback);
+    }
+
+    Kt.prototype.getOrderbook = function(item, limit, summarize, mode, callback) {
+        var params;
+        item = (typeof item === "string") ? item : item.id;
+        limit = limit || 10;
+        mode = mode || "both";
+        params = {
+            "item": item,
+            "limit": limit,
+            "mode": mode,
+            "summarize": summarize
+        }
+        this.performRequest("GET", "tsv", "orderbook/{item}", params, null, false, callback);
+    }
+    
     window.Kt = Kt;
+    window.KtAuthenticators = {
+        HmacAuthenticator: HmacAuthenticator
+    }
 }());
