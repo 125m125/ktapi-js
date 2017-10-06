100b031244fa4eaba04dee3d280d15c97a72b7c4
GPlay97 <GPlay97@users.noreply.github.com>
2017-10-07 00:52:42 +0200

    implement BasicAuth authenticator

--- ktapi.js
+++ ktapi.js
@@ -72,6 +72,27 @@
         return shaObj.getHMAC("HEX");
     };
 
+    /**
+     * Authenticator which will be used for BasicAuth
+     * @constructor BasicAuthenticator
+     */
+    function BasicAuthenticator() {
+        /**
+         * Function to authenticate
+         * @param  {String} method  the desired method to use for the request (e.g. GET)
+         * @param  {String} type    the data type (e.g. JSON)
+         * @param  {String} suburl  the suburl to use
+         * @param  {Object} params  params to use for the request
+         * @param  {Object} headers headers to use for the request
+         * @param  {Object} user    the user object
+         * @return {void}
+         */
+        this.authenticate = function(method, type, suburl, params, headers, user) {
+            // set header for authorization
+            if(!headers.Authorization) headers.Authorization = "Basic " + btoa(user.tid + ':' + user.tkn);
+        }
+    }
+
     function Request(method, type, suburl, params, headers, user, authenticator) {
         this.execute = function (callback) {
             if (!params) {
@@ -258,6 +279,7 @@
 
     window.Kt = Kt;
     window.KtAuthenticators = {
-        HmacAuthenticator: HmacAuthenticator
+        HmacAuthenticator: HmacAuthenticator,
+        BasicAuthenticator: BasicAuthenticator
     };
 }());
