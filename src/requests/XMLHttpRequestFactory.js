7ec85bfee2e1999109b383aab903ab5c631c6d18
GPlay97 <GPlay97@users.noreply.github.com>
2017-10-21 00:51:01 +0200

    implement XMLHttpRequest factory handler (#3)
    * implement XMLHttpRequest factory handler
    
    * special handling for tsv type within XMLHttpRequest factory
    
    * modify error return on XMLHttpRequest

---
+++ src/requests/XMLHttpRequestFactory.js
@@ -0,0 +1,51 @@
+import XMLHttpRequest from "XMLHttpRequest";
+import { default as paramsToQuery } from '../util/paramsToQuery.js';
+import { default as createRequestFactory} from './RequestFactory.js';
+
+/**
+ * Function which performs XMLHttpRequest based on given params
+ * @param  {String}   method    the method to use for this request (e.g. GET)
+ * @param  {String}   type      the type of the request
+ * @param  {String}   url       the url to use for this request
+ * @param  {Object}   params    params to use for this request
+ * @param  {Object}   headers   the headers to use for this request
+ * @param  {Function} callback  callback function
+ * @return {void}
+ */
+function performRequest(method, type, url, params, headers, callback) {
+    var request;
+
+    try {
+        request = new XMLHttpRequest();
+
+        // apply listener for the request
+        request.onreadystatechange = function() {
+            if (this.readyState == XMLHttpRequest.DONE) {
+                var response;
+                try {
+                    response = JSON.parse(this.responseText);
+                } catch (e) {
+                    return callback(this);
+                }
+                if(this.status >= 400) return callback(response);
+                return callback(null, ((type === 'tsv')? response.entries : response));
+            }
+        };
+        request.onerror = function(e) {
+            callback(e, null);
+        };
+        // prepare the request
+        request.open(method, url, true);
+        // apply headers
+        request.setRequestHeader("Accept", "application/json");
+        Object.keys(headers).forEach(function (key) {
+            request.setRequestHeader(key, headers[key]);
+        });
+        // send the request
+        request.send(paramsToQuery(params));
+    } catch (e) {
+        callback(e, null);
+    }
+};
+
+export default createRequestFactory(performRequest);
