ca3c972bca278444802ec15d69822dd49fbcf843
125m125 <125m125@125m125.de>
2017-12-23 01:01:42 +0100

    move name and globals to follow renaming in rollup config
    https://gist.github.com/Rich-Harris/d472c50732dab03efeb37472b08a3f32

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,16 @@
 export default {
     input: 'src/index.js',
-    name: 'Kt',
     output: {
+        name: 'Kt',
         file: 'target/ktapi-full.js',
-        format: 'umd'
+        format: 'umd',
+        globals: {
+            'jssha': 'jsSHA',
+            'd3-request': 'd3',
+            'pusher-js': 'Pusher',
+            'xmlhttprequest': 'XMLHttpRequest',
+            'btoa':'btoa'
+        },
     },
     external: ['jssha','d3-request','pusher-js', 'xmlhttprequest', 'btoa'],
-    globals: {
-        'jssha': 'jsSHA',
-        'd3-request': 'd3',
-        'pusher-js': 'Pusher',
-        'xmlhttprequest': 'XMLHttpRequest',
-        'btoa':'btoa'
-    }
 }
